1bfc3bd27f78f79959a3e261e988853e1505ad0c
Andrew Mcclellan <andrewmcclellan@Andrews-MacBook-Air.local>
2014-12-04 16:07:31 -0600

    Add transparent logos to image folder Add JavaScript event handlers to change logo backgrounds on hover Add links to logos

---
+++ scripts/amportfolio.js
@@ -0,0 +1,27 @@
+$(document).on('ready', function() {
+
+  $('#facebook').on('mouseover', function () {
+    $('#facebook').css("background", "#4c66a4")
+  })
+
+  $('#facebook').on('mouseout', function () {
+    $('#facebook').css("background", "black")
+  })
+
+  $('#linkedin').on('mouseover', function () {
+    $('#linkedin').css("background", "#D1D1D1")
+  })
+
+  $('#linkedin').on('mouseout', function () {
+    $('#linkedin').css("background", "black")
+  })
+
+  $('#email').on('mouseover', function () {
+    $('#email').css("background", "#B8E65C")
+  })
+
+  $('#email').on('mouseout', function () {
+    $('#email').css("background", "black")
+  })
+
+});

b2e7c1c4a45f901025aa31f75f0a9c13557f3bfe
Andrew Mcclellan <andrewmcclellan@Andrews-MacBook-Air.local>
2014-12-10 13:45:01 -0600

    Refactor project hover events in javascript file

--- scripts/amportfolio.js
+++ scripts/amportfolio.js
@@ -1,27 +1,46 @@
 $(document).on('ready', function() {
 
   $('#facebook').hover(
-    function() { colorChange($(this), "#4C66A4") },
-    function() { colorChange($(this), "#393939") }
+    function() { bgroundChange($(this), "#4C66A4") },
+    function() { bgroundChange($(this), "#393939") }
   );
 
   $('#linkedin').hover(
-    function() { colorChange($(this), "#0099CC") },
-    function() { colorChange($(this), "#393939") }
+    function() { bgroundChange($(this), "#0099CC") },
+    function() { bgroundChange($(this), "#393939") }
   );
 
   $('#email').hover(
-    function() { colorChange($(this), "black") },
-    function() { colorChange($(this), "#393939") }
+    function() { bgroundChange($(this), "black") },
+    function() { bgroundChange($(this), "#393939") }
   );
 
   $("#github").hover(
-    function() { colorChange($(this), "#B8E65C") },
-    function() { colorChange($(this), "#393939") }
+    function() { bgroundChange($(this), "#B8E65C") },
+    function() { bgroundChange($(this), "#393939") }
+  );
+
+  $(".rmc").hover(
+    function() { colorChange('.rmc_label', "#B8E65C") },
+    function() { colorChange('.rmc_label', "#393939") }
+  );
+
+  $(".shield_ED").hover(
+    function() { colorChange('.shield_ED_label', "#B8E65C") },
+    function() { colorChange('.shield_ED_label', "#393939") }
+  );
+
+  $(".roomack").hover(
+    function() { colorChange('.roomack_label', "#B8E65C") },
+    function() { colorChange('.roomack_label', "#393939") }
   );
 
 });
 
-  function colorChange(element, color) {
-    $(element).css("background", color);
-  };
+function bgroundChange(element, color) {
+  $(element).css("background", color);
+};
+
+function colorChange(element, color) {
+  $(element).css("color", color);
+};
